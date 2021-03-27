0c9532b3d6fb9beae5f214aa76433b1cf537cb3f
Ahmed111222333 <68826015+Ahmed111222333@users.noreply.github.com>
2021-03-27 13:25:03 +0200

    Ramadan Kareem

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var i = 0, text;
+var j = 0 , text2;
+var element = document.getElementById("ram");
+text = "رمــضــان كــريــم"
+text2 = "اللهم اجعل صيامي فيه صيام الصائمين وقيامي فيه قيام القائمين، ونبهني فيه عَن نومة الغافلين، وهب لي جرمي فيه يا رب العالمين، واعف عني يا عافيًا عن المجرمين" 
+function typing() {
+    if (i < text.length) {
+        document.getElementById("text").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typing, 90);
+        if (i==18) {
+    typing2();
+}
+
+    }
+}
+function typing2() {
+    if (j < text2.length) {
+        document.getElementById("text2").innerHTML += text2.charAt(j);
+        j++;
+        setTimeout(typing2, 90);
+        if (j==text2.length) {
+            element.classList.remove("d-none");
+            element.classList.add("d-block");
+        }
+    }
+}
+typing();
+console.log(text.length)
